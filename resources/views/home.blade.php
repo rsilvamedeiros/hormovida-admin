6e3a84d31ad86b1ec3b8000708098f6504e30e8f
Renan Medeiros <renanmedeiros79@gmail.com>
2023-07-17 16:05:53 -0300

    Correções iniciais do layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'AdminLTE')
+@section('title', 'HormoVida')
 
 @section('content')
 
@@ -13,7 +13,7 @@
     <section class="content">
 
       @cannot('user')
-        
+
       <!-- Info boxes -->
       <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
@@ -21,7 +21,7 @@
             <span class="info-box-icon bg-aqua"><i class="ion ion-ios-people-outline"></i></span>
             <div class="info-box-content">
               <span class="info-box-text">Quant. Usuários</span>
-            <span class="info-box-number">{{count($Usuarios)}}<small></small></span>
+              <span class="info-box-number">{{count($Usuarios)}}<small></small></span>
             </div>
           </div>
         </div>
@@ -60,11 +60,11 @@
         </div>
       </div>
 
-       <!-- LATERAL -->
-       
-          <!-- Info Boxes Style 2 -->
-          <div class="row">
-              <div class="col-md-3 col-sm-6 col-xs-12">
+      <!-- LATERAL -->
+
+      <!-- Info Boxes Style 2 -->
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
 
           <div class="info-box bg-yellow">
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
@@ -72,8 +72,8 @@
             <div class="info-box-content">
               <span class="info-box-text">Exames Solicitados</span>
               <span class="info-box-number">{{count($ExamesSolicitado)}}</span>
+            </div>
           </div>
-        </div>
           <div class="info-box bg-green">
             <span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
             <div class="info-box-content">
@@ -101,113 +101,112 @@
           </div>
         </div>
 
-    
 
 
-       <!-- fim da Info boxes -->
 
-       <!-- INICIO DA TABELA -->
+        <!-- fim da Info boxes -->
+
+        <!-- INICIO DA TABELA -->
 
         <div class="col-md-9">
           <div class="row">
 
-          <div class="box box-info">
-            <div class="box-header with-border">
-              <h3 class="box-title">Próximos Exames</h3>
+            <div class="box box-info">
+              <div class="box-header with-border">
+                <h3 class="box-title">Próximos Exames</h3>
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                <div class="box-tools pull-right">
+                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
               </div>
-            </div>
-            
-            <div class="box-body">
+
+              <div class="box-body">
                 <table id="lista1" class="table table-bordered table-striped dataTable" role="grid">
-                      <thead>
-                          <tr>
-                              <th>ID</th>
-                              <th>Paciente</th>
-                              <th>Médico</th>
-                              <th>Data Realizada</th>
-                              <th>Tipo Equipamento</th>
-                              <th>Staus</th>
-                              </tr>
-                      </thead>
-                      <tbody>
-                      @foreach($results as $x)
-                      <tr>
-                              <th scope="row">{{ $x->id }}</th>
-                              <td>{{ $x->paciente }}</td>
-                              <td>{{ $x->medico }}</td>
-                              <td>{{ $x->dataRealizada }}</td>
-                              <td>{{ $x->TipoEquipaments }}</td>
-                              <td> 
-                                  @if($x->status == 'Agendado')
-                                      <span class="label label-info">{{$x->status}}</span>
-                                  @elseif ($x->status == 'Realizado')
-                                      <span class="label label-success">{{$x->status}}</span>
-                                  @elseif ($x->status == 'Cancelado')
-                                      <span class="label label-danger">{{$x->status}}</span>
-                                  @elseif ($x->status == 'Solicitado')
-                                      <span class="label label-warning">{{$x->status}}</span>
-                                  @else
-                                      <span class="label label-default">{{$x->status}}</span>
-                                      @endif
-                              </td>
-                          <tr>
-                      @endforeach                           
-                      </tbody>
-                  </table>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Paciente</th>
+                      <th>Médico</th>
+                      <th>Data Realizada</th>
+                      <th>Tipo Equipamento</th>
+                      <th>Staus</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($results as $x)
+                    <tr>
+                      <th scope="row">{{ $x->id }}</th>
+                      <td>{{ $x->paciente }}</td>
+                      <td>{{ $x->medico }}</td>
+                      <td>{{ $x->dataRealizada }}</td>
+                      <td>{{ $x->TipoEquipaments }}</td>
+                      <td>
+                        @if($x->status == 'Agendado')
+                        <span class="label label-info">{{$x->status}}</span>
+                        @elseif ($x->status == 'Realizado')
+                        <span class="label label-success">{{$x->status}}</span>
+                        @elseif ($x->status == 'Cancelado')
+                        <span class="label label-danger">{{$x->status}}</span>
+                        @elseif ($x->status == 'Solicitado')
+                        <span class="label label-warning">{{$x->status}}</span>
+                        @else
+                        <span class="label label-default">{{$x->status}}</span>
+                        @endif
+                      </td>
+                    <tr>
+                      @endforeach
+                  </tbody>
+                </table>
 
-                  <div class="box-footer clearfix">
-                      <a href="{{route('Exam.add')}}" class="btn btn-sm btn-primary btn-flat">Criar um novo Exame</a>
-                      <a href="{{route('Exam.index')}}" class="btn btn-sm btn-warning btn-flat ">Ver todos os exames</a>
-                    </div>
+                <div class="box-footer clearfix">
+                  <a href="{{route('Exam.add')}}" class="btn btn-sm btn-primary btn-flat">Criar um novo Exame</a>
+                  <a href="{{route('Exam.index')}}" class="btn btn-sm btn-warning btn-flat ">Ver todos os exames</a>
+                </div>
 
-                      <div align="center">
-                          {!! $results->links() !!}
-                      </div> 
-              </div> 
+                <div align="center">
+                  {!! $results->links() !!}
+                </div>
+              </div>
               <!-- FIM DA div class="table-responsive"> -->
             </div>
             <!-- FIM DA box-header -->
+          </div>
         </div>
+
       </div>
-                        
-          </div>
-          <!-- FIM DA TABELA -->
-          </div>
+      <!-- FIM DA TABELA -->
+  </div>
 
-        </section>
-    </div>
+  </section>
+  </div>
 
-    
-    @endcannot('user')
-    
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap  -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- ChartJS -->
-<script src="bower_components/chart.js/Chart.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard2.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-  
+
+  @endcannot('user')
+
+  <!-- jQuery 3 -->
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- FastClick -->
+  <script src="bower_components/fastclick/lib/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+  <!-- Sparkline -->
+  <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+  <!-- jvectormap  -->
+  <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- SlimScroll -->
+  <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <!-- ChartJS -->
+  <script src="bower_components/chart.js/Chart.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="dist/js/pages/dashboard2.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+
 </body>
 
 @stop
-
